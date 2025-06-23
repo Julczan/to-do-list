b203245f26a3a3d49ad1f9434f8a50daacf0f2a5
Julczan <julek111222h@gmail.com>
2025-06-23 13:08:22 +0200

    sidebar elements

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,6 +1,17 @@
 function createSidebar() {
   const container = document.createElement("div");
   container.classList.add("sidebar");
+
+  const profile = document.createElement("div");
+  profile.classList.add("sidebar-element");
+  profile.textContent = "Julczan";
+
+  const projects = document.createElement("div");
+  projects.classList.add("sidebar-element");
+  projects.textContent = "My Projects";
+
+  container.appendChild(profile);
+  container.appendChild(projects);
   return container;
 }
 
