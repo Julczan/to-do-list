f9dfd0162151ec40afca4d93cfc0df10155a5d07
Julczan <julek111222h@gmail.com>
2025-06-30 14:54:26 +0200

    display todos

--- src/display.js
+++ src/display.js
@@ -3,33 +3,22 @@ import { projectList, getActiveProject } from "./todo";
 export function displayToDo() {
   let active = getActiveProject();
 
-  console.log(active);
-
-  loopActiveToDo(active);
-
   const display = document.querySelector(".display");
 
-  const toDoCard = document.createElement("div");
-  toDoCard.classList.add("card");
-
-  const titleDiv = document.createElement("div");
-  titleDiv.classList.add("title");
-
-  const dateDiv = document.createElement("div");
-  dateDiv.classList.add("date");
-
-  toDoCard.appendChild(titleDiv);
-  toDoCard.appendChild(dateDiv);
-
-  display.appendChild(toDoCard);
-}
-
-function loopActiveToDo(active) {
-  let toDo = [];
   for (let i = 0; i < active.length; i++) {
-    toDo = [active[i].toDoTitle, active[i].toDoDesc];
-  }
-  console.log(toDo);
+    const toDoCard = document.createElement("div");
+    toDoCard.classList.add("card");
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("title");
 
-  return toDo;
+    const dateDiv = document.createElement("div");
+    dateDiv.classList.add("date");
+
+    dateDiv.textContent = active[i].toDoDueDate;
+    titleDiv.textContent = active[i].toDoTitle;
+
+    toDoCard.appendChild(titleDiv);
+    toDoCard.appendChild(dateDiv);
+    display.appendChild(toDoCard);
+  }
 }
